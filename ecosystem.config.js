60a8fc6d77bd93678126a5ecd6e22a35e5545661
unknown <sam.nmeje@tabriz.ng>
2024-04-18 19:28:34 +0100

    update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    apps : [{
+  apps: [{
       name: "defacto-server",
-      script: "app.ts",
-      interpreter: "ts-node",
-      watch: true,
+      script: "app.ts",       // Your TypeScript file
+      interpreter: "ts-node", // Specifies ts-node as the interpreter
+      watch: true,            // Watch for file changes
       env: {
-        NODE_ENV: "development"
+          NODE_ENV: "development"
       },
       env_production: {
-        NODE_ENV: "production"
+          NODE_ENV: "production"
       }
-    }]
-  };
-  
+  }]
+};
