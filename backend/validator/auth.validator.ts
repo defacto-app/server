7a6bc98b2ed35b3c6728474de9def2c61f8bcd18
unknown <unknow@gmail.com>
2024-04-29 07:24:54 +0100

    zod is working

--- backend/validator/auth.validator.ts
+++ backend/validator/auth.validator.ts
@@ -1,21 +1,39 @@
 import { parsePhoneNumberFromString } from "libphonenumber-js";
+import { z } from "zod";
 
+const PhoneNumberSchema = z.object({
+   phoneNumber: z.string().refine(value => value.trim() !== '' && value.trim().length >= 10, {
+      message: 'Phone number must not be empty and must have at least 10 digits.',
+   }),
+});
 
 export default {
    validPhoneNumber: async function(body: any) {
-      const phoneNumber = body.phoneNumber;
 
-      // Input validation (optional):
-      if (!phoneNumber || typeof phoneNumber !== 'string') {
-         throw new Error('Invalid phone number format. Please provide a string value.');
+
+      const result = PhoneNumberSchema.safeParse(body);
+      if (!result.success) {
+         const formattedErrors: any = {};
+         result.error.errors.forEach((error) => {
+            const fieldName = error.path[0];
+            formattedErrors[fieldName] = error.message;
+         });
+
+         console.log("formattedErrors", formattedErrors);
+         return { data: null, error: formattedErrors };
       }
+      console.log("parsed", result);
+
+
+      const { phoneNumber } = result.data;
+
       try {
+
          const parsedNumber = parsePhoneNumberFromString(phoneNumber, "NG"); // 'NG' is the country code for Nigeria
 
 
-
          if (!parsedNumber?.isValid()) {
-            throw new Error('Invalid phone number. Please check the format and ensure it follows Nigerian standards.');
+            throw new Error("Invalid phone number. Please check the format and ensure it follows Nigerian standards.");
          }
 
          const data = {
