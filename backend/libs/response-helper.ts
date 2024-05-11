3ede52846e090f405f0e38c164d9746ca3efe5ad
Katalyst33 <44523904+Katalyst33@users.noreply.github.com>
2024-05-11 02:47:18 +0100

    update time format in logger

--- backend/libs/response-helper.ts
+++ backend/libs/response-helper.ts
@@ -1,28 +1,41 @@
 import { Response } from "express";
 import logger from "../../config/logger";
+import moment from "moment";
+
+function formatAMPM(date: Date): string {
+      return moment().format("MMMM Do YYYY, h:mm:ss A");
+  
+
+}
 
 class SendResponse {
    /**
     * Send a success response.
     */
-   static success(res: Response,  message: string, data?: any,) {
+   /**
+ * Format a date object to AM/PM timestamp format.
+ */
+
+
+   static success(res: Response, message: string, data?: any,) {
       return res.status(200).json({
          success: true,
          message,
          data,
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
+
       });
    }
 
    /**
     * Send a generic error response with status 400.
     */
-   static badRequest(res: Response, message: string,error?: any) {
+   static badRequest(res: Response, message: string, error?: any) {
       return res.status(400).json({
          success: false,
          message,
          error,
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
       });
    }
 
@@ -34,7 +47,7 @@ class SendResponse {
       return res.status(401).json({
          success: false,
          message,
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
       });
    }
 
@@ -45,7 +58,7 @@ class SendResponse {
       return res.status(404).json({
          success: false,
          message,
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
       });
    }
 
@@ -57,18 +70,18 @@ class SendResponse {
       return res.status(500).json({
          success: false,
          message: "An unexpected error occurred",
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
       });
    }
 
    static serviceUnavailable(res: Response, message: string, error?: any) {
 
-     logger.error(error);
+      logger.error(error);
       return res.status(503).json({
          success: false,
          message,
          error,
-         timestamp: new Date()
+         timestamp: formatAMPM(new Date())
       });
    }
 }
