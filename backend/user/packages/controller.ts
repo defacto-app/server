4487819e3e3c51b709403f502e94066b7ccd009a
Katalyst33 <44523904+Katalyst33@users.noreply.github.com>
2024-05-20 03:03:25 +0100

    project rquired field

--- backend/user/packages/controller.ts
+++ backend/user/packages/controller.ts
@@ -14,11 +14,24 @@ const PackageController = {
       const page: number = parseInt(req.query.page as string) || 1;
       const perPage: number = parseInt(req.query.perPage as string) || 10;
      const query = { userId: user.userId };
-      const projection = { cashAvailable: 0 }; // Exclude password field
+     const projection = {
+      "dropOffDetails._id": 0,
+      "dropOffDetails.label": 0, // Ensure this field exists or remove if not necessary
+      "dropOffDetails.createdAt": 0,
+      "dropOffDetails.updatedAt": 0,
+      "dropOffDetails.userId": 0,
+      "pickupDetails._id": 0,
+      "pickupDetails.label": 0, // Ensure this field exists or remove if not necessary
+      "pickupDetails.userId": 0,
+      "pickupDetails.createdAt": 0,
+      "pickupDetails.updatedAt": 0,
+      "userId": 0,
+      "_id": 0,
+      "__v": 0, // Exclude the version key if not already done by default
+    };
       try {
         const paginationResult = await paginate(PackageModel, page, perPage, query, projection);
         
-        res.json(paginationResult);
 
         SendResponse.success(res, "Packages retrieved", paginationResult);
       } catch (error:any) {
