2c970d02733a02c15424a5b943da5c1b223357c1
sam.nmeje <sam@katalyst.tech>
2024-10-04 23:02:07 +0100

    added ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,9 @@ module.exports = {
          script: "./app.ts",
          instances: "max", // Scales to maximum available CPUs
          exec_mode: "cluster", // Allows running in cluster mode for load balancing
-         watch: true, // Watch for changes in files and automatically restart the app
+         watch: false, // Watch for changes in files and automatically restart the app
          ignore_watch: ["node_modules", "logs","combined.log"], // Ignore specific folders
+         max_memory_restart: "500M",
          env: {
             NODE_ENV: "development",
             PORT: 5700,
